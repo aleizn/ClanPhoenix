03f545ebda083655afab5ab2fb0d54504a6bd80c
aleiznbot <118560958+aleiznbot@users.noreply.github.com>
2023-01-02 11:47:25 -0500

    Bot creado por Wa.me/51914865141
    Bot creado por Wa.me/51914865141

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,11 +4,13 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let oi = `*𝐋𝐥𝐚𝐦𝐚𝐝𝐨 𝐆𝐞𝐧𝐞𝐫𝐚𝐥:* ${pesan}`
+let teks = `*⺀𝐄𝐓𝐈𝐐𝐔𝐄𝐓𝐀 - 𝐏𝐇𝐎𝐄𝐍𝐈𝐗⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└* 𝐁𝐲 𝐍𝐨𝐭𝐉𝐡𝐮𝐳ᙆ  ᷦ\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `└└@ᴄʟᴀɴᴘʜᴏᴇɴɪxғʀᴇᴇғɪʀᴇ
+└@ᴄʟᴀɴᴘʜᴏᴇɴɪxғᴇᴍ_ғғ
+└@ᴘʜᴏᴇɴɪx_ᴇsᴘᴏʀᴛs_sᴜᴅ`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
